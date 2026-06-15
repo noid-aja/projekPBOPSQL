3a5fdb6d107f5fb247543055202bef4ce1ec83f6
USER <USER@DESKTOP-B1RSMOD>
2026-06-16 01:23:40 +0700

    indonesia raya - wr supratman

--- Forms/AdminForm/KelolaUser.cs
+++ Forms/AdminForm/KelolaUser.cs
@@ -321,5 +321,15 @@ namespace WinFormsApp1.Forms.AdminForm
         {
 
         }
+
+        private void cmbFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
